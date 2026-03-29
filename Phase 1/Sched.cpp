f4d5bec1d57a30000b96174afd9e2f32278ff18e
Colin Christy <colinchristy15@gmail.com>
2026-03-29 00:51:27 -0400

    Fixed formatting of Scheduler files and create_task creates a thread

--- Phase 1/Sched.cpp
+++ Phase 1/Sched.cpp
@@ -1,19 +1,77 @@
 #include "Sched.h"
 
-int main() {
-    Scheduler scheduler;
-
-    scheduler.create_task("Task1");
-    scheduler.create_task("Task2");
-    scheduler.create_task("Task3");
-    scheduler.create_task("Task4");
-
-    scheduler.dump(1);
-    for (int i = 0; i < 1000000000; i++) {
-        int j = i + i * i;
-        if (i % 100000000 == 0) {
-            scheduler.create_task("Task" + std::to_string(i/100000000));
-        }
-    }
-    scheduler.dump(1);
+Scheduler::Scheduler()
+{
+    process_table = nullptr;    // No processes yet
+    current_task = -1;          // No task is running
+    current_quantum = 500;      // Set quantum length to 500ms
 }
+
+Scheduler::~Scheduler()
+{
+    std::cout << "Exiting Scheduler....." << std::endl;
+}
+
+int Scheduler::create_task(std::string task_name, void *(*task_function)(void *))
+{
+    TCB *new_task = new TCB();
+
+    new_task->task_name = task_name;
+    new_task->state = READY;
+    new_task->start_time = clock();
+    new_task->next = process_table;
+    process_table = new_task;
+
+    new_task->task_id = pthread_create(&(new_task->thread), nullptr, task_function, (void*)1);
+
+    return new_task->task_id;
+}
+
+void Scheduler::dump(int level)
+{
+    if (level != 1 && level != 2) // If not expected level, print error and exit
+    {
+        std::cerr << level << " is an invalid level" << std::endl;
+        return;
+    }
+
+    std::cout << "-----------Process Table-----------" << std::endl;
+
+    TCB *current_task = process_table;
+    int count = 0;
+    while (current_task != nullptr)
+    {
+        std::cout << std::setw(7) << current_task->task_name
+                  << std::setw(7) << current_task->task_id
+                  << std::setw(7) << current_task->state
+                  << std::setw(7) << current_task->start_time
+                  << std::endl;
+
+        current_task = current_task->next;
+        count++;
+    }
+
+    std::cout << "-----------------------------------" << std::endl;
+}
+
+// This testing should be handled in Ultima.cpp  \/
+// int main()
+// {
+//     Scheduler scheduler;
+
+//     scheduler.create_task("Task1");
+//     scheduler.create_task("Task2");
+//     scheduler.create_task("Task3");
+//     scheduler.create_task("Task4");
+
+//     scheduler.dump(1);
+//     for (int i = 0; i < 1000000000; i++)
+//     {
+//         int j = i + i * i;
+//         if (i % 100000000 == 0)
+//         {
+//             scheduler.create_task("Task" + std::to_string(i / 100000000));
+//         }
+//     }
+//     scheduler.dump(1);
+// }
--- Phase 1/Sched.h
+++ Phase 1/Sched.h
@@ -1,87 +1,86 @@
-/*
-Sched.h
-
-Header file to contain the TCB struct, Scheduler class, constants, and function signatures for the Sched.cpp file.
-
-Colin Christy
-March 26, 2026
-Last update: March 26, 2026
-*/
+/**
+ * Sched.h
+ *
+ * Basic scheduler class that handles managing tasks.
+ *
+ * @author Colin Christy
+ * @date 3/28/2026
+ */
 
 #include <string>
 #include <iostream>
 #include <iomanip>
+#include <pthread.h>
 
-const std::string READY     = "READY";
-const std::string RUNNING   = "RUNNING";
-const std::string BLOCKED   = "BLOCKED";
-const std::string DEAD      = "DEAD";
+const std::string READY = "READY";
+const std::string RUNNING = "RUNNING";
+const std::string BLOCKED = "BLOCKED";
+const std::string DEAD = "DEAD";
 
-const int MAX_TASKS         = 100;
+const int MAX_TASKS = 100;
 
-struct TCB {
+struct TCB
+{
     int task_id;
     std::string task_name;
     std::string state;
     clock_t start_time;
+    pthread_t thread;
     TCB *next;
 };
 
-class Scheduler {
+/**
+ * Basic scheduler class that handles managing tasks.
+ */
+class Scheduler
+{
+private:
     TCB *process_table;
-
     int current_task;
     long current_quantum;
-    int next_available_task_id;
 
 public:
-    Scheduler() {
-        process_table           = nullptr;  // No processes yet
-        current_task            = -1;       // No task is running
-        next_available_task_id  = 0;        // Start ids at 0
-        current_quantum         = 500;      // Set quantum length to 500ms
-    }
-    ~Scheduler() {
-        std::cout << "Exiting Scheduler....." << std::endl;
-    }
-    int create_task(std::string task_name) {
-        TCB *new_task = new TCB();
+    /**
+     * Default constructor
+     */
+    Scheduler();
 
-        new_task->task_id = next_available_task_id++;
-        new_task->task_name = task_name;
-        new_task->state = READY;
-        new_task->start_time = clock();
-        new_task->next = process_table;
-        process_table = new_task;
+    /**
+     * Destructor
+     */
+    ~Scheduler();
 
-        return new_task->task_id;
-    };
-    bool kill_task() {
-        return false; // Returns if the operation was successful
-    }
-    void yield() {
+    /**
+     * Creates a task and creates a TCB in the process table.
+     * 
+     * @param task_name Name of the task being created.
+     * @param task_function Function that the task will execute.
+     */
+    int create_task(std::string task_name, void *(*task_function)(void *));
 
-    };
-    void garbage_collect() {
+    /**
+     * Kills the specified task.
+     * 
+     * @param task_id Id of the task to be killed.
+     */
+    bool kill_task(int task_id);
 
-    };
+    /**
+     * Attempts to give the scheduler back control from the task. The scheduler could
+     * decide to run a different task or continue to run the same task.
+     */
+    void yield();
 
-    void dump(int level) {
-        std::cout << "-----------Process Table-----------" << std::endl;
+    /**
+     * Cleans up dead tasks from the process table that do not need to be tracked any longer.
+     */
+    void garbage_collect();
 
-        TCB *current_task = process_table;
-        int count = 0;
-        while (current_task != nullptr) {
-            std::cout << std::setw(7) << current_task->task_name 
-            << std::setw(7) << current_task->task_id
-            << std::setw(7) << current_task->state
-            << std::setw(7) << current_task->start_time
-            << std::endl;
-
-            current_task = current_task->next;
-            count++;
-        }
-
-        std::cout << "-----------------------------------" << std::endl;
-    };
+    /**
+     * Prints the process table with various info about the current state of all tasks.
+     * Use the level parameter to change how much detail is given.
+     * 
+     * @param level Amount of detail shown (Default: 2)
+     */
+    void dump(int level = 2);
 };
